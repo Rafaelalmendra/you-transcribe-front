80b2152049730166c53db6c661a82e88bfdf9ae8
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-02-16 23:12:57 -0300

    feat: launch home ui

--- src/app/components/shared/github/index.tsx
+++ src/app/components/shared/github/index.tsx
@@ -10,12 +10,17 @@ import {
 // icons
 import { Github } from "lucide-react";
 
-const GithubLinks = () => {
+type GithubLinksProps = {
+  hasText?: boolean;
+};
+
+const GithubLinks = ({ hasText }: GithubLinksProps) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size={hasText ? "default" : "icon"}>
           <Github size={18} />
+          {hasText && <span className="ml-2">Github</span>}
           <span className="sr-only">Acessar Github</span>
         </Button>
       </DropdownMenuTrigger>
--- src/app/views/home/index.tsx
+++ src/app/views/home/index.tsx
@@ -1,11 +1,38 @@
 import { useTranslations } from "next-intl";
 
+// components
+import { Button, GithubLinks, Input } from "components";
+
+// icons
+import { PlayCircle } from "lucide-react";
+
 const HomeView = () => {
   const t = useTranslations("Home");
 
   return (
-    <div>
-      <div></div>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-[3.5rem] font-bold text-[#FAFAFA] mb-1">
+        {t("title")}
+      </h1>
+
+      <p className="max-w-[651px] text-center text-[24px] text-[#A1A1AA] mb-5">
+        {t("description")}
+      </p>
+
+      <GithubLinks hasText />
+
+      <div className="flex items-center gap-2 mt-[26px]">
+        <Input
+          placeholder="Adicione a URL do vídeo aqui"
+          className="w-[410px] h-[40px]"
+        />
+        <Button className="font-medium bg-gradient-to-r from-[#3AC4E2] to-[#E4EBC7] text-[#09090B] gap-[10px]">
+          <PlayCircle size={20} />
+          {t("button")}
+        </Button>
+      </div>
+
+      <p className="mt-[30px] text-[#A1A1AA]">🎉 {t("subtitle")}</p>
     </div>
   );
 };
